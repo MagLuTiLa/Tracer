0f645afe7dae47a6bd07943ce31903929b3a14c2
maguell <maglutila@gmail.com>
2016-11-24 16:11:18 +0100

    Comment changes

--- game.cpp
+++ game.cpp
@@ -41,15 +41,7 @@ void Game::Tick( float dt )
 	// Clear the screen
 	//screen->Clear( 0 );
 
-	// Camera
-	
-
-	// One sphere
-	// TODO replace with vector of primitives
-	//Sphere sphere (vec3(0, 0, 5), 1.0f);
-	//sphere.location += vec3(0.01f, -0.01f, -0.01f);
-
-	// One poinlight 
+	// One pointlight 
 	// TODO replace with vector of pointlights
 	//PointLight pl(vec3(0,0,5), 0x999999);
 
@@ -66,8 +58,7 @@ void Game::Tick( float dt )
 			if ((int)x == SCRWIDTH / 2 && (int)y == SCRHEIGHT / 2)
 				int a = 0;
 
-			// See if ray intersects with sphere
-			// TODO iterate over vector of primitives and only work with closest intersection
+			// See if ray intersects with primtives
 			for (std::vector<Primitive>::size_type i = 0; i != primitives.size(); i++)
 			{
 				Primitive* p = primitives[i];
@@ -84,8 +75,7 @@ void Game::Tick( float dt )
 				glm::vec3 rayPos = ray.direction * (ray.length - 0.0001f);
 				Ray shadowRay (rayPos, glm::normalize(pl.location - rayPos), std::numeric_limits<float>::max());
 
-				// See if shadow ray intersects with sphere
-				// TODO iterate over vector of primitives and break upon first intersection (if intersection distance is smaller than distance to light source)
+				// See if shadow ray intersects with primitives
 				for (std::vector<Primitive>::size_type i = 0; i != primitives.size(); i++)
 				{
 					Primitive* p = primitives[i];
