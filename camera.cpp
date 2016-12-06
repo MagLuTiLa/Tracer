ab492e887537d7f2d73942b272e09fe6aa76ecd3
maguell <maglutila@gmail.com>
2016-12-06 04:35:00 +0100

    Adjusted camera screen to make it more general

--- camera.cpp
+++ camera.cpp
@@ -2,9 +2,10 @@
 #include "camera.h"
 
 
-Camera::Camera(glm::vec3 p, glm::vec3 lookAt, float dist):
+Camera::Camera(glm::vec3 p, glm::vec3 lookAt, glm::vec3 u, float dist):
 	position(p),
-	distance(dist)
+	distance(dist),
+	up(u)
 {
 	direction = glm::normalize(lookAt - position);
 	Update();
@@ -13,23 +14,38 @@ Camera::Camera(glm::vec3 p, glm::vec3 lookAt, float dist):
 void Camera::Update()
 {
 	center = position + direction * distance;
+	right = glm::cross(up, direction);
+	down = -glm::cross(direction, right);
 
 	float stdValue = 1.0f;
-	float ratio;
+	glm::vec3 ratio;
 	if (SCRWIDTH > SCRHEIGHT)
 	{
-		ratio = (float)SCRWIDTH / (float)SCRHEIGHT;
-		topLeft = center + glm::vec3(-ratio, -1, 0);
+		float r = ((float)SCRWIDTH / (float)SCRHEIGHT);
+		ratio = r * right;
+
+		topLeft = center - ratio - down;
+		topRight = center + ratio - down;
+		botLeft = center - ratio + down;
+		/*topLeft = center + glm::vec3(-ratio, -1, 0);
 		topRight = center + glm::vec3(ratio, -1, 0);
-		botLeft = center + glm::vec3(-ratio, 1, 0);
+		botLeft = center + glm::vec3(-ratio, 1, 0);*/
 	}
 	else
 	{
-		ratio = (float)SCRHEIGHT / (float)SCRWIDTH;
-		topLeft = center + glm::vec3(-1, -ratio, 0);
+		ratio = ((float)SCRHEIGHT / (float)SCRWIDTH) * down;
+
+		topLeft = center - right - ratio;
+		topRight = center + right - ratio;
+		botLeft = center - right + ratio;
+		/*topLeft = center + glm::vec3(-1, -ratio, 0);
 		topRight = center + glm::vec3(1, -ratio, 0);
-		botLeft = center + glm::vec3(-1, ratio, 0);
+		botLeft = center + glm::vec3(-1, ratio, 0);*/
 	}
+	/*
+	topLeft = center - right - down;
+	topRight = center + right - down;
+	botLeft = center - right + down;*/
 
 	// Used for the "screen" through where rays are shot
 	width = topRight - topLeft;
--- camera.h
+++ camera.h
@@ -2,7 +2,7 @@
 class Camera
 {
 public:
-	Camera(glm::vec3, glm::vec3, float);
+	Camera(glm::vec3, glm::vec3, glm::vec3, float);
 	Ray ShootRay(float u, float v);
 	void Update();
 	void MoveForward();
@@ -10,8 +10,12 @@ public:
 
 	glm::vec3 position;
 	glm::vec3 direction;
+	glm::vec3 up;
+
 	float distance;
 	glm::vec3 center;
+	glm::vec3 right;
+	glm::vec3 down;
 
 	glm::vec3 topLeft;
 	glm::vec3 topRight;
